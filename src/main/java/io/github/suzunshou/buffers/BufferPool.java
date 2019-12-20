c9111e52b6cc5a24253857f72f3418a3c6a2e9e5
suzunshou <suzunshou@meituan.com>
2019-12-20 14:17:23 +0800

    fix

--- src/main/java/io/github/suzunshou/buffers/BufferPool.java
+++ src/main/java/io/github/suzunshou/buffers/BufferPool.java
@@ -1,5 +1,7 @@
 package io.github.suzunshou.buffers;
 
+import java.lang.reflect.Array;
+
 /*
  * @author suzunshou 2019-12-19 20:29:17
  * A buffer pool which keeps a free list of direct buffers of a specified default
@@ -7,12 +9,12 @@ package io.github.suzunshou.buffers;
  * If the stack is full the buffer is de-referenced and available to be
  * freed by normal garbage collection.
  */
-public class BufferPool {
+public class BufferPool<T> {
 
     private int buffersInPool = 0;
     private int maxPoolEntries;
     private int bufferSize;
-    private Buffer[] pool;
+    private Buffer<T>[] pool;
 
     public BufferPool(int maxPoolEntries, int bufferSize) {
         if (maxPoolEntries <= 0) {
@@ -20,15 +22,15 @@ public class BufferPool {
         }
         this.maxPoolEntries = maxPoolEntries;
         this.bufferSize = bufferSize;
-        pool = new Buffer[maxPoolEntries];
+        pool = (Buffer<T>[]) Array.newInstance(Buffer.class, maxPoolEntries);
     }
 
-    public Buffer acquire() {
+    public Buffer<T> acquire() {
         return takeBufferFromPool();
     }
 
-    private Buffer takeBufferFromPool() {
-        Buffer result = null;
+    private Buffer<T> takeBufferFromPool() {
+        Buffer<T> result = null;
         synchronized (this) {
             if (buffersInPool > 0) {
                 buffersInPool -= 1;
@@ -36,16 +38,16 @@ public class BufferPool {
             }
         }
         if (result == null) {
-            result = new Buffer(bufferSize);
+            result = new Buffer<>(bufferSize);
         }
         return result;
     }
 
-    public void release(Buffer buffer) {
+    public void release(Buffer<T> buffer) {
         offerBufferToPool(buffer);
     }
 
-    private void offerBufferToPool(Buffer buffer) {
+    private void offerBufferToPool(Buffer<T> buffer) {
         synchronized (this) {
             if (buffersInPool < maxPoolEntries) {
                 pool[buffersInPool] = buffer;
